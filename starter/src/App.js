2db5363cf8e2a2c87e69a2ad73bf4077d8c51ee4
Thu Tran <erictr0922@gmail.com>
2022-11-24 23:11:13 -0500

    add redux toolkit

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 export default App;
